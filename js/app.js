d2f953ca29ad96387daf8da8e60b83149a2e7264
dwqkwqdwqodpk <kapateli2019@gmail.com>
2024-06-09 11:38:33 +0300

    language

--- js/app.js
+++ js/app.js
@@ -12,3 +12,34 @@ mclose.addEventListener('click', function () {
 	mwrap.classList.toggle('mobile__wrapper-active')
 	body.classList.toggle('hide')
 })
+
+
+
+
+$(document).ready(function () {
+	const translates = {
+		'ru': {
+			'title': 'RU',
+			'language': 'Русский',
+		},
+		'en': {
+			'title': 'EN',
+			'language': 'English',
+		},
+	}
+
+	let currentLang = 'ru'
+
+	const elements = $('[data-langkey]')
+
+	$('.language').on('click', () => {
+		currentLang = currentLang === 'ru' ? 'en' : 'ru'
+		elements.each((key, el) => {
+			const obj = $(el)
+			const langKey = obj.data('langkey')
+			obj.text(translates[currentLang][langKey])
+		})
+	})
+
+
+})
